cd49ef23ac4b23c805dd21bca6c8bc0d2bc25e94
Tushar866913 <115552768+Tushar866913@users.noreply.github.com>
2022-12-10 23:54:01 +0530

    Add files via upload

---
+++ assignment-3/js/assign-3.js
@@ -0,0 +1,290 @@
+
+
+var maindiv=document.getElementById('main-div1')
+// div-1 start
+var cont1=document.createElement('div')
+maindiv.insertBefore(cont1,maindiv[0]);
+cont1.classList.add('div-1')
+cont1.classList.add('media')
+
+ var a = document.createElement('img');
+ a.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg';
+ a.classList.add('picture');
+ cont1.appendChild(a);
+
+ var b = document.createElement('p');
+ b.innerHTML='Men Navy Blue Solid Sweatshirt'
+ b.classList.add('para');
+ cont1.appendChild(b);
+
+ var c = document.createElement('p');
+ c.innerHTML='United Colors of Benetton'
+ c.classList.add('heading');
+ cont1.appendChild(c);
+
+ var d = document.createElement('p');
+ d.innerHTML='Price: Rs 2599'
+ d.classList.add('heading2');
+ cont1.appendChild(d);
+
+// div-1 end
+
+
+// // div-2 start
+var cont2=document.createElement('div')
+maindiv.insertBefore(cont2,maindiv[1]);
+cont2.classList.add('div-1')
+cont2.classList.add('media')
+
+ var e = document.createElement('img');
+ e.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2060340/2017/9/14/11505387708574-Puma-Men-Black-Solid-Sporty-Jacket-371505387708496-1.jpg';
+ e.classList.add('picture');
+ cont2.appendChild(e);
+
+ var f = document.createElement('p');
+ f.innerHTML='Men Black MAMGP T7 Sweat Sporty Jacket'
+ f.classList.add('para');
+ cont2.appendChild(f);
+
+ var g = document.createElement('p');
+ g.innerHTML='Puma'
+ g.classList.add('heading');
+ cont2.appendChild(g);
+
+ var h = document.createElement('p');
+ h.innerHTML='Price: Rs 7999'
+ h.classList.add('heading2');
+ cont2.appendChild(h);
+// // div-2 end
+
+//div-3 start
+var cont3=document.createElement('div')
+maindiv.insertBefore(cont3,maindiv[2]);
+cont3.classList.add('div-1')
+cont3.classList.add('media')
+
+ var i = document.createElement('img');
+ i.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2018/9/19/b1373b00-37d4-41a0-a908-5d742a68d3661537343756229-1.jpg';
+ i.classList.add('picture');
+ cont3.appendChild(i);
+
+ var j = document.createElement('p');
+ j.innerHTML='Men Black Action Parkview Lifestyle Shoes'
+ j.classList.add('para');
+ cont3.appendChild(j);
+
+ var k = document.createElement('p');
+ k.innerHTML='Hush Puppies'
+ k.classList.add('heading');
+ cont3.appendChild(k);
+
+ var l = document.createElement('p');
+ l.innerHTML='Price: Rs 6999'
+ l.classList.add('heading2');
+ cont3.appendChild(l)
+
+//div-3 end
+
+//div-4start
+var cont3=document.createElement('div')
+maindiv.insertBefore(cont3,maindiv[2]);
+cont3.classList.add('div-1')
+cont3.classList.add('media')
+
+ var i = document.createElement('img');
+ i.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/3854721/2018/3/12/11520844673915-BARESKIN-Women-Black-Solid-Lightweight-Leather-Jacket-7761520844673718-1.jpg';
+ i.classList.add('picture');
+ cont3.appendChild(i);
+
+ var j = document.createElement('p');
+ j.innerHTML='Women Black Solid Lightweight Leather Jacket'
+ j.classList.add('para');
+ cont3.appendChild(j);
+
+ var k = document.createElement('p');
+ k.innerHTML='BARESKIN'
+ k.classList.add('heading');
+ cont3.appendChild(k);
+
+ var l = document.createElement('p');
+ l.innerHTML='Price: Rs 9999'
+ l.classList.add('heading2');
+ cont3.appendChild(l)
+//div-4 end
+
+//div-5 start
+var cont3=document.createElement('div')
+maindiv.insertBefore(cont3,maindiv[2]);
+cont3.classList.add('div-1')
+//  cont3.classList.add('media')
+cont3.style.clear='both'
+
+ var i = document.createElement('img');
+ i.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/5649908/2018/5/10/6bfe80cd-2f55-42bc-aa7f-e0d6c9e2ac531525936414747-SASSAFRAS-Women-Blue-Solid-Shirt-Dress-3831525936414532-1.jpg';
+ i.classList.add('picture');
+ cont3.appendChild(i);
+
+ var j = document.createElement('p');
+ j.innerHTML='Women Blue Solid Shirt Dress'
+ j.classList.add('para');
+ cont3.appendChild(j);
+
+ var k = document.createElement('p');
+ k.innerHTML='SASSAFRAS'
+ k.classList.add('heading');
+ cont3.appendChild(k);
+
+ var l = document.createElement('p');
+ l.innerHTML='Price: Rs 5200'
+ l.classList.add('heading2');
+ cont3.appendChild(l)
+//div-5 end
+
+
+//--------------------------------------------------------------------------------------------------------------
+
+
+var maindiv2=document.getElementById('main-div2')
+// div-6 start
+var cont6=document.createElement('div')
+maindiv2.insertBefore(cont6,maindiv2[0]);
+cont6.classList.add('div-1')
+cont6.classList.add('media')
+
+ var p = document.createElement('img');
+ p.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803247/2019/5/27/624dac60-4c42-4902-bba1-30a51cc7f43c1558948536543-Apple-Unisex-Smart-Watches-1441558948536225-1.jpg';
+ p.classList.add('picture');
+ cont6.appendChild(p);
+
+ var q = document.createElement('p');
+ q.innerHTML='Unisex Silver-Toned Series 3 Smart Watch'
+ q.classList.add('para');
+ cont6.appendChild(q);
+
+ var r = document.createElement('p');
+ r.innerHTML='Apple'
+ r.classList.add('heading');
+ cont6.appendChild(r);
+
+ var s = document.createElement('p');
+ s.innerHTML='Price: Rs 31999'
+ s.classList.add('heading2');
+ cont6.appendChild(s);
+
+ //div-6 end
+
+
+ //div-7 start
+
+ var cont6=document.createElement('div')
+ maindiv2.insertBefore(cont6,maindiv2[0]);
+ cont6.classList.add('div-1')
+ cont6.classList.add('media')
+ 
+  var p = document.createElement('img');
+  p.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2019/2/23/dc05b5e5-aa82-493e-bf0b-0c309baf21021550920383101-1.jpg';
+  p.classList.add('picture');
+  cont6.appendChild(p);
+ 
+  var q = document.createElement('p');
+  q.innerHTML='Unisex Black & Green Reflex 2.0 Smart Band'
+  q.classList.add('para');
+  cont6.appendChild(q);
+ 
+  var r = document.createElement('p');
+  r.innerHTML='Fastrack'
+  r.classList.add('heading');
+  cont6.appendChild(r);
+ 
+  var s = document.createElement('p');
+  s.innerHTML='Price: Rs 1999'
+  s.classList.add('heading2');
+  cont6.appendChild(s);
+
+ //div-7 end
+
+
+ //div-8 start
+
+ var cont6=document.createElement('div')
+ maindiv2.insertBefore(cont6,maindiv2[0]);
+ cont6.classList.add('div-1' )
+ cont6.classList.add('media')
+ 
+  var p = document.createElement('img');
+  p.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/10122083/2019/6/24/e533a691-3908-41b0-8307-1928a37d4ec41561362162650-Samsung-Galaxy-Fit-4801561362161527-1.jpg';
+  p.classList.add('picture');
+  cont6.appendChild(p);
+ 
+  var q = document.createElement('p');
+  q.innerHTML='Unisex Black Galaxy Fit Fitness Band'
+  q.classList.add('para');
+  cont6.appendChild(q);
+ 
+  var r = document.createElement('p');
+  r.innerHTML='Samsung'
+  r.classList.add('heading');
+  cont6.appendChild(r);
+ 
+  var s = document.createElement('p');
+  s.innerHTML='Price: Rs 9990'
+  s.classList.add('heading2');
+  cont6.appendChild(s);
+ //div-8 end
+
+
+ //div-9 start
+ var cont6=document.createElement('div')
+maindiv2.insertBefore(cont6,maindiv2[0]);
+cont6.classList.add('div-1')
+cont6.classList.add('media')
+
+ var p = document.createElement('img');
+ p.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/8593217/2019/4/9/de2c21f3-6731-4c29-9369-692a486a1b8c1554802772926-Samsung-Gear-IconX-Black-Cord-free-Fitness-Earbuds-256155480-1.jpg';
+ p.classList.add('picture');
+ cont6.appendChild(p);
+
+ var q = document.createElement('p');
+ q.innerHTML='Gear IconX Black Cord-free Fitness Earbuds'
+ q.classList.add('para');
+ cont6.appendChild(q);
+
+ var r = document.createElement('p');
+ r.innerHTML='Samsung'
+ r.classList.add('heading');
+ cont6.appendChild(r);
+
+ var s = document.createElement('p');
+ s.innerHTML='Price: Rs 13990'
+ s.classList.add('heading2');
+ cont6.appendChild(s);
+ //div-9 end
+
+ //div-10 start
+ var cont6=document.createElement('div')
+maindiv2.insertBefore(cont6,maindiv2[0]);
+cont6.classList.add('div-1')
+cont6.classList.add('media')
+
+ var p = document.createElement('img');
+ p.src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803279/2019/5/27/6661d579-12ae-456b-b8f2-e78e5256a99c1558948436173-Apple-AirPods-with-Charging-Case-2nd-Gen-White-3831558948435-1.jpg';
+ p.classList.add('picture');
+ cont6.appendChild(p);
+
+ var q = document.createElement('p');
+ q.innerHTML='White 2nd Gen AirPods with Charging Case'
+ q.classList.add('para');
+ cont6.appendChild(q);
+
+ var r = document.createElement('p');
+ r.innerHTML='Apple'
+ r.classList.add('heading');
+ cont6.appendChild(r);
+
+ var s = document.createElement('p');
+ s.innerHTML='Price: Rs 14999'
+ s.classList.add('heading2');
+ cont6.appendChild(s);
+ //div-10 end
+
+
